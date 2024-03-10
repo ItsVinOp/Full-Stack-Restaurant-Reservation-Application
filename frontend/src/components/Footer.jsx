207b7299f6e492ec75edbc5bf575ca94a7732eea
ItsVinOp <117188366+ItsVinOp@users.noreply.github.com>
2024-03-10 20:42:22 +0530

    first commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">ItsVinOp</div>
+          <div className="right">
+            <p>India Gate, India</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By VinOP</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By ItsVinOp.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
